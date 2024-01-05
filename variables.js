50957cdeed982f938423b2b3c8797d10b7978426
Samardh Kanugula <samardh30@gmail.com>
2024-01-04 16:33:42 -0800

    Landing Page

---
+++ variables.js
@@ -0,0 +1,13 @@
+function variables() {
+
+    let userinput;
+    userinput = document.getElementById("user").value;
+
+    let passinput;
+    passinput = document.getElementById("pass").value;
+
+    let GFG_Var;
+ 
+    userdisplay = (userinput);
+    document.getElementById('result').innerHTML = `${userdisplay}` ;
+    }
